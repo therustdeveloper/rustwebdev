49ec85f676b0934124d4bbb45f16a7afb0fa6f05
william <william@godeveloper.io>
2023-10-21 12:35:12 -0500

    Initial structure of the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(
+                Error::new(ErrorKind::InvalidInput, "No id provided")
+            ),
+        }
+    }
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()])
+    );
+
+    println!("{:?}", question);
 }
