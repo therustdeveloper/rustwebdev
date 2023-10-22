a67fdf1a9804affee3899e8cdf96b7cb654818eb
william <william@godeveloper.io>
2023-10-22 16:04:54 -0500

    implement warp framework

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
+use serde::Serialize;
+use warp::Filter;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -9,7 +11,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl FromStr for QuestionId {
@@ -36,7 +38,7 @@ impl Question {
     }
 }
 
-fn main() {
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
@@ -44,5 +46,21 @@ fn main() {
         Some(vec!["faq".to_string()])
     );
 
-    println!("{:?}", question);
+    Ok(warp::reply::json(
+        &question
+    ))
+}
+
+#[tokio::main]
+async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+
+    let routes = get_items;
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
